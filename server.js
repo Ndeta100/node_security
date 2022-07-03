f87b7a80f73d51a6be3c34d0da6cb107428808b3
Ndeta Innocent <you@example.com>
2022-07-03 16:17:44 +0300

    installing free ssl with openssl

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const path=require('path')
 const express=require('express')
+const https=require('https')
+const path=require('path')
+const fs=require('fs')
 const app=express()
 const PORT=3000
 const secret=Math.floor(Math.random()*100)
@@ -10,6 +12,9 @@ app.get('/', (req,res)=>{
   return  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, ()=>{
+https.createServer({
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem')
+}, app).listen(PORT, ()=>{
     console.log(`App running on port ${PORT}`)
 })
